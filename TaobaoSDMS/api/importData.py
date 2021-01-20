be9a2c09371f93d3dead9c306501df86298798bc
zhangwx0794 <zhangwx0794@163.com>
2021-01-20 22:38:49 +0800

    a

---
+++ TaobaoSDMS/api/importData.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/1/18 0018 10:35
+# @Author : Owen
+# @Email : zhangwx0794@gmail.com
+# @File : main.py
+# @Project : Taobao
+from api.ClsTaobao import *
+import os
+import time
+import shutil
+
+
+if __name__ == '__main__':
+
+    # 调用测试方法
+    # testFunc()
+    # exit(0)
+    # 初始化taobao对象
+    taobao = Taobao()
+    # 创建工作目录
+
+    if taobao.getSystemPlatform() == 'Windows':
+        if not os.path.exists(os.getcwd()+'\\work'):
+            os.mkdir(os.getcwd()+'\\work')
+    else:
+        if not os.path.exists(os.getcwd() + '/work'):
+            os.mkdir(os.getcwd() + '/work')
+
+    # 定义工作目录和程序主目录
+    mainDir = os.getcwd()
+    os.chdir('work')
+    workDir = os.getcwd()
+    os.chdir(mainDir)
+
+
+    # 备份工作目录
+    print('【备份工作目录】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+    tmid = str(time.strftime('%Y_%m_%d_%H_%M_%S'))
+    workBakDir = 'work-' + tmid
+    os.mkdir(workBakDir)
+    os.chdir(workBakDir)
+    workBakAbsDir = os.getcwd()
+    os.chdir(mainDir)
+    os.removedirs(workBakAbsDir)
+    print(workDir,workBakAbsDir)
+    shutil.copytree(workDir,workBakAbsDir)
+    print('【备份工作目录】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+
+    # 定义xls文件存放目录
+    xlsDir = workDir + '\\'
+    xlsList = []
+
+    # if taobao.getSystemPlatform() == 'Windows':
+    #     # 1. xls转xlsx
+    #     # 获取工作目录下所有xls文件名称
+    #     xlsList = taobao.get_path_xls(xlsDir)
+    #     print('【xls转xlsx】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+    #     for xls in xlsList:
+    #         # 拼接xls绝对路径
+    #         xlsAbsPath = xlsDir + xls
+    #         taobao.xls_to_xlsx(xlsAbsPath)
+    #     print('【xls转xlsx】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+    # 2. 检查文件命名规范，重命名文件
+    # 获取工作目录下所有xlsx文件名称
+    xlsxList = taobao.get_path_xlsx(xlsDir)
+    print('【检查文件命名规范&重命名】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+    for xlsx in xlsxList:
+        xlsxAbsPath = xlsDir + xlsx
+        taobao.format_xls_name(xlsxAbsPath)
+    print('【检查文件命名规范&重命名】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    # 3. 删除文件中所有订单号都在数据库中已存在的文件
+    xlsxList = taobao.get_path_xlsx(xlsDir)
+    print('【删除文件】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+    for xlsx in xlsxList:
+        xlsxAbsPath = xlsDir + xlsx
+        # 删除订单号为空的行
+        taobao.delBlankOrderRow(xlsxAbsPath)
+        # * 打开excel
+        wb = xlrd.open_workbook(xlsxAbsPath)
+        # * 打开第一个sheet
+        ws = wb.sheet_by_index(0)
+        cnt = taobao.chkXlsOrderUniq(xlsxAbsPath)
+        if int(ws.nrows)-1 == cnt:
+            os.remove(xlsxAbsPath)
+            print('所有订单号均存在于数据库中，已删除文件',xlsxAbsPath)
+    print('【删除文件】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    # 4. 导入数据
+    # 获取工作目录下所有xlsx文件名称
+    xlsxList = taobao.get_path_xlsx(xlsDir)
+    print('【文件导入】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+    for xlsx in xlsxList:
+        xlsxAbsPath = xlsDir + xlsx
+        print(xlsxAbsPath,'处理中……')
+        # 删除订单号为空的行
+        taobao.delBlankOrderRow(xlsxAbsPath)
+        cnt = int(taobao.importData(xlsxAbsPath))
+        if cnt > 0:
+            os.remove(xlsxAbsPath)
+            print('订单导入完毕，删除',xlsxAbsPath)
+    print('【文件导入】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
--- TaobaoSDMS/app.py
+++ TaobaoSDMS/app.py
@@ -3,13 +3,18 @@ from flask import Flask, render_template, request, redirect, url_for, session, s
 from werkzeug.utils import secure_filename
 from api.mysql_func import *
 from api.zhangwx import *
+import api.importData
 import xlsxwriter
 import uuid
 import io
 import os
-import time,datetime
+import time, datetime
 from dateutil.relativedelta import relativedelta
 import xlwt, xlrd
+from api.ClsTaobao import *
+import os
+import time
+import shutil
 
 app = Flask(__name__)
 
@@ -274,11 +279,10 @@ def repeTaskCheck():
                 uploadPath = uploadDir + '/' + file.filename
             # uploadPath = os.path.join(basepath, 'static\\uploads', secure_filename(file.filename))
             file.save(uploadPath)
-
             wb = xlrd.open_workbook(uploadPath)
             # * 打开第一个sheet
             ws = wb.sheet_by_index(0)
-            repeTaskList=[]
+            repeTaskList = []
             for line in range(1, ws.nrows):
                 wangWangId = str(ws.cell_value(rowx=line, colx=0)).strip()
                 shopName = str(ws.cell_value(rowx=line, colx=1)).strip()
@@ -293,16 +297,134 @@ def repeTaskCheck():
                 # 删除用户上传的任务检查表
                 os.remove(uploadPath)
                 if len(sqlRes) > 0:
-                    tp = (wangWangId,shopName,taskDate,sqlRes[0][2],sqlRes[0][3])
+                    tp = (wangWangId, shopName, taskDate, sqlRes[0][2], sqlRes[0][3])
                     repeTaskList.append(tp)
-                    checkRes = [1,repeTaskList]
+                    checkRes = [1, repeTaskList]
                 else:
                     checkRes = [-1, '恭喜你，本excel中所有旺旺ID 1个月内无重复任务！']
-
-
     elif request.method == 'GET':
         checkRes = [-1, '']
     return render_template('repeTaskCheck.html', checkRes=checkRes)
+
+
+@app.route("/importData", methods=["GET", 'POST'])
+def importData():
+    cnt = 0
+    if request.method == 'POST':
+        file = request.files.get("file")
+        if file.filename is '':
+            # 表示没有发送文件
+            checkRes = [-1, '错误，未上传文件']
+        elif str(file.filename).split('.')[-1] not in ('xls', 'xlsx'):
+            checkRes = [-1, '错误，文件类型不是xls或者xlsx']
+        else:
+            os.getcwd()
+            basepath = os.getcwd()  # 当前文件所在路径
+            if getSystemPlatform() == 'Windows':
+                uploadDir = basepath + '\\work'
+            else:
+                uploadDir = basepath + '/work'
+            if not os.path.exists(uploadDir):
+                os.mkdir(uploadDir)
+            if getSystemPlatform() == 'Windows':
+                uploadPath = uploadDir + '\\' + file.filename
+            else:
+                uploadPath = uploadDir + '/' + file.filename
+            # uploadPath = os.path.join(basepath, 'static\\uploads', secure_filename(file.filename))
+            file.save(uploadPath)
+            # 初始化taobao对象
+            taobao = Taobao()
+            # 创建工作目录
+
+            if taobao.getSystemPlatform() == 'Windows':
+                if not os.path.exists(os.getcwd() + '\\work'):
+                    os.mkdir(os.getcwd() + '\\work')
+            else:
+                if not os.path.exists(os.getcwd() + '/work'):
+                    os.mkdir(os.getcwd() + '/work')
+
+            # 定义工作目录和程序主目录
+            mainDir = os.getcwd()
+            os.chdir('work')
+            workDir = os.getcwd()
+            os.chdir(mainDir)
+
+            # 备份工作目录
+            print('【备份工作目录】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+            tmid = str(time.strftime('%Y_%m_%d_%H_%M_%S'))
+            workBakDir = 'work-' + tmid
+            os.mkdir(workBakDir)
+            os.chdir(workBakDir)
+            workBakAbsDir = os.getcwd()
+            os.chdir(mainDir)
+            os.removedirs(workBakAbsDir)
+            print(workDir, workBakAbsDir)
+            shutil.copytree(workDir, workBakAbsDir)
+            print('【备份工作目录】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+            # 定义xls文件存放目录
+            xlsDir = workDir + '\\'
+            xlsList = []
+
+            # # 1. xls转xlsx
+            # # 获取工作目录下所有xls文件名称
+            # xlsList = taobao.get_path_xls(xlsDir)
+            # print('【xls转xlsx】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+            # for xls in xlsList:
+            #     # 拼接xls绝对路径
+            #     xlsAbsPath = xlsDir + xls
+            #     taobao.xls_to_xlsx(xlsAbsPath)
+            # print('【xls转xlsx】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+            # 2. 检查文件命名规范，重命名文件
+            # 获取工作目录下所有xlsx文件名称
+            xlsxList = taobao.get_path_xlsx(xlsDir)
+            print('【检查文件命名规范&重命名】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+            for xlsx in xlsxList:
+                xlsxAbsPath = xlsDir + xlsx
+                taobao.format_xls_name(xlsxAbsPath)
+            print('【检查文件命名规范&重命名】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+            # 3. 删除文件中所有订单号都在数据库中已存在的文件
+            xlsxList = taobao.get_path_xlsx(xlsDir)
+            print('【删除文件】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+            for xlsx in xlsxList:
+                xlsxAbsPath = xlsDir + xlsx
+                # 删除订单号为空的行
+                taobao.delBlankOrderRow(xlsxAbsPath)
+                # * 打开excel
+                wb = xlrd.open_workbook(xlsxAbsPath)
+                # * 打开第一个sheet
+                ws = wb.sheet_by_index(0)
+                cnt = taobao.chkXlsOrderUniq(xlsxAbsPath)
+                if int(ws.nrows) - 1 == cnt:
+                    os.remove(xlsxAbsPath)
+                    print('所有订单号均存在于数据库中，已删除文件', xlsxAbsPath)
+            print('【删除文件】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+            # 4. 导入数据
+            # 获取工作目录下所有xlsx文件名称
+            xlsxList = taobao.get_path_xlsx(xlsDir)
+            print('【文件导入】工作开始', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+            for xlsx in xlsxList:
+                xlsxAbsPath = xlsDir + xlsx
+                print(xlsxAbsPath, '处理中……')
+                # 删除订单号为空的行
+                taobao.delBlankOrderRow(xlsxAbsPath)
+                cnt = int(taobao.importData(xlsxAbsPath))
+                if cnt > 0:
+                    os.remove(xlsxAbsPath)
+                    print('订单导入完毕，删除', xlsxAbsPath)
+            print('【文件导入】工作结束', time.strftime('%Y-%m-%d %H:%M:%S'))
+            importData = [('', '', '', '', '', '', '', '', '', '', '', '', '', '')]
+            # 如果是从POST来的请求，并且导入0条数据，那么赋值cnt=-1 表示导入失败
+            if cnt == 0:
+                cnt = -1
+    elif request.method == 'GET':
+        importData = [('', '', '', '', '', '', '', '', '', '', '', '', '', '')]
+        cnt = 0
+    return render_template('importData.html', importData=importData,cnt=cnt)
 
 
 if __name__ == '__main__':
